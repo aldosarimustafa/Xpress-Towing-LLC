101ddd3fc383c3073e91021eddfa476323d35cf9
aldosarimustafa <84543087+aldosarimustafa@users.noreply.github.com>
2021-08-15 23:35:55 -0700

    add service API

---
+++ src/utilities/service-api.js
@@ -0,0 +1,24 @@
+import sendRequest from './send-requests';
+
+const BASE_URL = '/api/service';
+
+export function createService(serviceData) {
+    return sendRequest(BASE_URL, 'POST', serviceData);
+}
+
+export function getOne(serviceId) {
+    return sendRequest(`${BASE_URL}/${serviceId}`);
+}
+
+export function sendservice(serviceId, service) {
+    console.log(serviceId, service);
+    return sendRequest(`${BASE_URL}/${serviceId}/send`, 'POST', service);
+}
+
+export function getAll() {
+    return sendRequest(BASE_URL);
+}
+
+export function deleteService(serviceId) {
+    return sendRequest(`${BASE_URL}/${serviceId}`, 'DELETE')
+}
